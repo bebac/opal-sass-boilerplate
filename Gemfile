2984e8dc3f80475dd90df416862ca98f06b89dcb
Benny Bach <benny.bach@gmail.com>
2017-04-21 20:25:56 +0200

    Update opal to 0.10.3 and remove vienna

--- Gemfile
+++ Gemfile
@@ -1,9 +1,7 @@
 source 'https://rubygems.org'
 
-#gem 'opal',         :github => 'opal/opal', :branch => 'master'
-gem 'opal',         :github => 'opal/opal', :tag => "v0.9.2"
+gem 'opal',         :github => 'opal/opal', :tag => "v0.10.3"
 gem 'opal-browser', :github => 'opal/opal-browser'
-gem 'opal-vienna',  :github => 'bebac/vienna', :branch => 'opal-browser'
 gem 'sass'
 
 gem 'therubyracer'
--- source/application.rb
+++ source/application.rb
@@ -1,26 +1,9 @@
 require 'opal'
 require 'browser'
-require 'browser/effects'
-require 'vienna'
-
-require_tree 'templates'
-require_tree 'views'
 
 class Application
   def run
-    router.update
-    puts "hello"
-  end
-
-  def router
-    @router ||= Vienna::Router.new.tap do |router|
-      #router.route('/')                    { |params| router.navigate('albums') }
-      #router.route('menu/:action(/:state)') { |params| @view.show_content(params) }
-    end
-  end
-
-  def find(selector)
-    $document.at(selector)
+    $document.at('body').inner_html = "<h1>Hello, World</h1>"
   end
 end
 
